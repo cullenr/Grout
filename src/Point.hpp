1bf879b5ce900cdad82b1e8cd4a7a91144e2a5b3
cullenr <rory@lexer.cc>
2012-12-10 12:13:12 -0800

    added simple point struct

---
+++ src/Point.hpp
@@ -0,0 +1,15 @@
+#ifndef POINT_H
+#define POINT_H
+
+namespace math{
+    struct Point{
+        float x;
+        float y;
+
+        Point()
+        {
+            x = y = 0;
+        }
+    };
+};
+#endif // POINT_H
